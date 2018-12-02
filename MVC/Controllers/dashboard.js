b652f3e6c9df269a98dd9b127f221205d6d9929e
Industrys AFH <andres_felipe0929@hotmail.com>
2018-12-01 23:49:56 -0500

    Dashboard responses 100%

--- MVC/Controllers/dashboard.js
+++ MVC/Controllers/dashboard.js
@@ -4,12 +4,74 @@ const Adoption = require("../Models/Adoption");
 
 module.exports = {
   getAllDataforDashboard: async (req, res) => {
-    const animals = await Animal.find({}).count();
-    const users = await User.find({}).count();
-    const adoption = await Adoption.find({}).count();
+    // ===========================================
+    const animals = await Animal.find({});
+    const animalsNoAdopted = animals.filter(currentValue => {
+      return currentValue.status === "enabled";
+    });
+    const animalsPending = animals.filter(currentValue => {
+      return currentValue.status === "pending";
+    });
+    const animalsAdopted = animals.filter(currentValue => {
+      return currentValue.status === "adopted";
+    });
+    const dogs = animals.filter(currentValue => {
+      return currentValue.animal === "dog";
+    });
+    const cats = animals.filter(currentValue => {
+      return currentValue.animal === "cat";
+    });
+
+    // ===========================================
+    const users = await User.find({});
+    const userAdministrators = users.filter(currentValue => {
+      return currentValue.rol === "administrator";
+    });
+    const userGuest = users.filter(currentValue => {
+      return currentValue.rol === "guest";
+    });
+
+    // ===========================================
+    const adoptionList = await Adoption.find({}).populate("animal");
+    const adoption = await Adoption.find({});
+    const adoptionPending = adoption.filter(currentValue => {
+      return currentValue.confirmed === false;
+    });
+    const adoptionTrue = adoption.filter(currentValue => {
+      return currentValue.confirmed === true;
+    });
 
     return res.status(200).json({
-      data: { animals, users, adoption }
+      data: {
+        dataForAnimals: {
+          enabled: Number(animalsNoAdopted.length),
+          pending: Number(animalsPending.length),
+          adopted: Number(animalsAdopted.length),
+          total:
+            Number(animalsNoAdopted.length) +
+            Number(animalsPending.length) +
+            Number(animalsAdopted.length),
+          type: {
+            dogs: Number(dogs.length),
+            cats: Number(cats.length)
+          }
+        },
+        dataForUsers: {
+          administrators: Number(userAdministrators.length),
+          guests: Number(userGuest.length),
+          total:
+            Number(`${userGuest.length} `) +
+            Number(`${userAdministrators.length}`)
+        },
+        dataForAdoptions: {
+          true: adoptionTrue.length,
+          false: adoptionPending.length,
+          total:
+            Number(`${adoptionTrue.length} `) +
+            Number(`${adoptionPending.length}`),
+          adoptions: adoptionList
+        }
+      }
     });
   }
 };
